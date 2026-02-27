be381b966e2af62c6fde4ceef2bd8a3cde520671
ornich <144551045+ornich@users.noreply.github.com>
2026-02-27 08:55:56 +0530

    Rename palindrome checker app and update messages

--- UseCase1PalindromeCheckerApp.java
+++ UseCase1PalindromeCheckerApp.java
@@ -1,26 +1,59 @@
-// UseCase1PalindromeCheckerApp.java
+// UseCase7PalindromeCheckerApp.java
 
-// Class acts as a container for the application logic
-public class UseCase1PalindromeCheckerApp {
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-    // Application version (static variable)
+public class UseCase7PalindromeCheckerApp {
+
     static final String APP_VERSION = "1.0";
 
-    // Main method - Entry point of the Java application
     public static void main(String[] args) {
 
-        // Display Welcome Message
-        System.out.println("======================================");
-        System.out.println("     WELCOME TO PALINDROME CHECKER    ");
-        System.out.println("======================================");
+        Scanner scanner = new Scanner(System.in);
 
-        // Display Application Details
+        System.out.println("======================================");
+        System.out.println("     DEQUE BASED PALINDROME CHECK     ");
+        System.out.println("======================================");
         System.out.println("Application Name : Palindrome Checker App");
         System.out.println("Application Version : " + APP_VERSION);
+        System.out.println("======================================");
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Create Deque
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear until empty
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();  // Remove from front
+            char rear = deque.removeLast();    // Remove from rear
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("Result: The string is a PALINDROME.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
 
         System.out.println("======================================");
-        System.out.println("Application Started Successfully...");
-        System.out.println("Ready for Palindrome Processing.");
-        System.out.println("======================================");
+        System.out.println("Program Ended Successfully.");
+
+        scanner.close();
     }
 }
